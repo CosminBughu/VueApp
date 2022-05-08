eb243b9f0435cd89b54feea8f67181ee9a361e02
Cosmin Ionut <cosminionut7@gmail.com>
2022-05-09 01:48:26 +0300

    Added connection to api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,57 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form @submit.prevent="createUser" method="post">
+      <div>
+        <label for="userName">userName</label>
+        <input type="text" v-model="formData.UserName">
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input type="text" v-model="formData.Email">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" v-model="formData.Password">
+      </div>
+      <div>
+        <label for="confirmedPassword">Confirm Password</label>
+        <input type="password" v-model="formData.ConfirmPassword">
+      </div>
+      <button>Submit</button>
+      {{users}}
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      formData:{
+        UserName: '',
+        Email: '',
+        Password: '',
+        ConfirmPassword: '',
+      },
+      users: null
+    }
+  },
+  mounted() {
+     axios.get('https://localhost:7006/api/account/users')
+       .then((users)=> console.log(users))
+        .catch((error)=> console.log(error))
+  },
+  methods: {
+    createUser(){
+      const data = JSON.stringify(this.formData);
+      const options = {headers: {"content-type": "application/json"}}
+      axios.post('https://localhost:7006/api/account/signup', data,options)
+        .then((response)=> console.log(response))
+        .catch((error)=> console.log(error))
+    },
+  },
 }
 </script>
 
